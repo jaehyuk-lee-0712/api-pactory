e8b5735c838adf9a071438caeedfd5c37796c53a
jaehyuk-lee-0712 <57936553+jaehyuk-lee-0712@users.noreply.github.com>
2024-05-14 16:46:27 +0900

    css check

--- php/login/login.php
+++ php/login/login.php
@@ -19,13 +19,13 @@
 <body>
     <div id="wrap">
 <?php
-    include "../component/headInfo.php";
+
     include "../component/header.php";
 
 ?>
         <div>
             <!-- login form -->
-            <form action="" id="loginForm">
+            <!-- <form action="" id="loginForm">
                 <article id="main">
                     <div class="container">
                         <div class="login">
@@ -66,8 +66,51 @@
                         </div>
                     </div>
                 </article>
-            </form>
+            </form> -->
             <!-- // login form end -->
+            
+            <!-- // signup form start -->
+            <form id="signInForm" action="signUpForm.php" name="signUpForm" method="post">
+                <div class="login">
+                    <div class="nav">
+                        <ul>
+                            <li><a href="#">SIGNUP</a></li>
+                        </ul>
+                    </div>
+                    <div class="Userbox">
+                        <label for="userEmail" class="required">이메일</label>
+                        <div class="check">
+                            <input type="email" name="userEmail" id="userEmail" placeholder="이메일을 입력하세요."
+                                autocomplete="off" class="input-style" required>
+                        </div>
+                        <p class="msg"></p>
+                    </div>
+                    <div class="Userbox">
+                        <label for="userName" class="required">이름</label>
+                        <div class="check">
+                            <input type="text" name="userName" id="userName" placeholder="이름을 입력하세요."
+                                autocomplete="off" class="input-style" required>
+                        </div>
+                    </div>
+                    <div class="Userbox">
+                        <label for="userPass" class="required">비밀번호</label>
+                        <div class="check">
+                            <input type="password" name="userPass" id="userPass" placeholder="비밀번호를 입력하세요."
+                                autocomplete="off" class="input-style" required>
+                        </div>
+                    </div>
+                    <div class="Userbox">
+                        <label for="userBirth" class="required">생년월일</label>
+                        <div class="check">
+                            <input type="text" name="userBirth" id="userBirth" placeholder="공백없이 8자리를 입력하세요."
+                                autocomplete="off" class="input-style" required>
+                        </div>
+                    </div>
+                    <input type="submit" class="signup-btn" value="회원가입">
+                </div>
+            </form>
+
+            <!-- // signup form end -->
         </div>
         </div>
     </div>
