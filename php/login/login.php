9599b5272dba8f27a4b47a86682696f1e836c30a
머원 <sdw10195@gmail.com>
2024-05-16 19:46:26 +0900

    0

--- php/login/login.php
+++ php/login/login.php
@@ -70,7 +70,7 @@
             <!-- // login form end -->
             
             <!-- // signup form start -->
-            <form id="signInForm" action="signUpForm.php" name="signUpForm" method="post">
+            <form id="signInForm" action="signUpForm.php" name="signUpForm" method="post" onsubmit="return joinChecks()">
                 <div class="login">
                     <div class="nav">
                         <ul>
@@ -109,7 +109,7 @@
                         </div>
                         <p class="msg" id="userBirthComment"></p>
                     </div>
-                    <input type="submit" class="signup-btn" onclick = "emailChecking()" value="회원가입">
+                    <input type="submit" class="signup-btn" onclick="emailChecking()" value="회원가입">
                 </div>
             </form>
 
@@ -155,7 +155,7 @@ function emailChecking() {
         dataType: "json",
         success: function(data) {
             if (data.result == "good") {
-                $("#userEmailComment").text("➟ 사용 가능한 이메일입니다.");
+                // $("#userEmailComment").text("➟ 사용 가능한 이메일입니다.");
                 isEmailCheck = true;
                 // 중복 여부 확인 후 회원가입 실행
                 signUp();
@@ -166,13 +166,71 @@ function emailChecking() {
         }
     });
 }
+        function joinChecks() {
+            // 메세지 초기화
+            $(".msg").text("");
+
+            let userEmail = $("#userEmail").val();
+            let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+            if (userEmail === '') {
+                $("#userEmailComment").text("➟ 이메일을 입력해주세요.");
+                $("#userEmail").focus();
+                return false;
+            } else if (!emailPattern.test(userEmail)) {
+                $("#userEmailComment").text("➟ 유효한 이메일 주소를 입력해주세요.");
+                $("#userEmail").focus();
+                return false;
+            }
+            
+            // 이름 검사
+            let userName = $("#userName").val();
+            let namePattern = /^[가-힣]{2,5}$/;
+
+            if (userName === '') {
+                $("#userNameComment").text("➟ 이름을 입력해주세요.");
+                $("#userName").focus();
+                return false;
+            } else if (!namePattern.test(userName)) {
+                $("#userNameComment").text("➟ 이름은 2~5글자의 한글로만 가능합니다.");
+                $("#userName").focus();
+                return false;
+            }
+
+            // 비밀번호 검사
+            let userPass = $("#userPass").val();
+            let passPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{4,20}$/; // 최소 4자, 최대 20자, 하나 이상의 문자, 숫자, 특수문자 포함
+
+            if (userPass === '') {
+                $("#userPassComment").text("➟ 비밀번호를 입력해주세요.");
+                $("#userPass").focus();
+                return false;
+            } else if (!passPattern.test(userPass)) {
+                $("#userPassComment").text("➟ 비밀번호는 4~20자의 영문자, 숫자, 특수문자를 포함해야 합니다.");
+                $("#userPass").focus();
+                return false;
+            }
+
+            // 연락처 검사
+            let userBirth = $("#userBirth").val();
+            let BirthPattern = /^(19|20)\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])$/;
+
+            if (userBirth === '') {
+                $("#userBirthComment").text("➟ 생년월일을 입력해주세요.");
+                $("#userBirth").focus();
+                return false;
+            } else if (!BirthPattern.test(userBirth)) {
+                $("#userBirthComment").text("➟ 유효한 생년월일을 입력해주세요. 공백, -없이 8자리");
+                $("#userBirth").focus();
+                return false;
+            }
+}
 
 function signUp() {
     if (!isEmailCheck) {
         alert("가입 실패!");
     } else {
-        // 여기에 회원가입 코드 추가
-        alert("회원가입을 축하합니다.!")
+        // alert("회원가입을 축하합니다.!");
     }
 }
 
