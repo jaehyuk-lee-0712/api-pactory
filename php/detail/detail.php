bf721e8e2f84737afc17b10553f1009491952f6d
skadbstj12 <163810643+skadbstj12@users.noreply.github.com>
2024-05-24 12:48:40 +0900

    41221

---
+++ php/detail/detail.php
@@ -0,0 +1,96 @@
+<?php
+
+include "../connection/connection.php";
+
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <?php include "../component/head.php"; ?>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+    <?php include "../component/header.php"; ?>
+    <main id="main" role="main">
+        <div class="detail-container">
+            <div class="detail__inner">
+                <img src="../../assets/img/banner1.jpg" alt="이미지">
+                <div class="detail__title">
+                <h2>카카오 api</h2>
+                <p>제작자 | 올린 날짜</p>
+                </div>
+                <div class="detail__box">
+                    <form action="#" id="tutorials-container" method ="post"class="tutorials-container">
+                        <div class="tutorial-header">
+                            <div class="th-Title">Api</div>
+                            <button id="add-btn" class="add-btn" type="button">
+                                add
+                            </button>
+                        </div>
+                      <div class="scroll">
+                      <div class="T-text">
+                        <div class="T-title"><p>X-RapidAPI-Key</p>
+                      <div class="tutorial-code"><span>123412341234123412341234</span></div></div>
+                        <div class="T-title"><p>X-RapidAPI-Host</p>
+                    <div class="tutorial-code"><span>youtube-v31.p.rapidapi.com</span></div></div>
+                    <div class="T-title"><p>X-RapidAPI-Key</p>
+                      <div class="tutorial-code"><span>123412341234123412341234</span></div></div>
+                        <div class="T-title"><p>X-RapidAPI-Host</p>
+                    <div class="tutorial-code"><span>youtube-v31.p.rapidapi.com</span></div></div>
+                    <div class="T-title"><p>X-RapidAPI-Key</p>
+                      <div class="tutorial-code"><span>123412341234123412341234</span></div></div>
+                        <div class="T-title"><p>X-RapidAPI-Host</p>
+                    <div class="tutorial-code"><span>youtube-v31.p.rapidapi.com</span></div></div>
+                    <div class="T-title"><p>X-RapidAPI-Key</p>
+                      <div class="tutorial-code"><span>123412341234123412341234</span></div></div>
+                        <div class="T-title"><p>X-RapidAPI-Host</p>
+                    <div class="tutorial-code"><span>youtube-v31.p.rapidapi.com</span></div></div>
+                    </select>
+                      </div>
+                      <div></div>
+                      </div>
+                    </form>
+                    <form action="#" id="code-container" method ="post"class="code-container">
+                    <div class="code-header">
+                    <div class="Code-title">Code Sources</div>
+                    </div>
+                   <div class="scroll2">
+                   <select name="#" id="code-language">
+                        <option value="1">노드</option>
+                        <option value="2">자바</option>
+                        <option value="3">몰라</option>
+                    </select>
+                    <div class="C-text">
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf asdfadsfadsfadsfdsafadsfadsfadsfdsafsf asdfadsfadsfadsfdsafadsfadsfadsfdsafsf asdfadsfadsfadsfdsafadsfadsfadsfdsafsf asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+
+                        asdfadsfadsfadsfdsafadsfadsfadsfdsafsf asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                         asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                          asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                          asdfadsfadsfadsfdsafadsfadsfadsfdsafsf
+                    </div>
+                   </div>
+                    </form>
+                </div>
+            </div>   
+        </div>
+    </main>
+    <!-- //main -->
+    <?php include "../component/footer.php"; ?>
+
+</body>
+</html>
