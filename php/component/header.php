bf0bfac6f02dbd14d32ca9497c54dbeb65f2890f
jaehyuk-lee-0712 <57936553+jaehyuk-lee-0712@users.noreply.github.com>
2024-06-05 14:56:43 +0900

    0605 Jaehyuk lee Commit

--- php/component/header.php
+++ php/component/header.php
@@ -1,4 +1,16 @@
 <header id="header">
+<?php
+     $currentDir = __DIR__;
+     $rootDir = $_SERVER['DOCUMENT_ROOT'];
+     $relativePath = str_replace($rootDir, '', $currentDir);
+     $relativePath = str_replace('\\', '/', $relativePath);
+     $relativePath = rtrim($relativePath, '/');
+ 
+     $loginPath = $relativePath . '/../login/';
+     $logoutPath = $loginPath . 'logOutAction.php';
+     $loginActionPath = $loginPath . 'login.php?action=login';
+     $signupActionPath = $loginPath . 'login.php?action=signup';
+?>
     <div id="logos">
         <div id="logo"></div>
         <div id="title">
@@ -7,6 +19,7 @@
         <div id="menu">
             <ul>
                 <li><a href="../product/product.php">api 상품</a></li>
+                <li><a href="#">카테고리</a></li>
                 <li><a href="../board/board.php">공지사항</a></li>
             </ul>
         </div>
@@ -15,34 +28,107 @@
         <div id="searchBar">
             <i class="fas fa-search"></i>
             <input type="text" placeholder="검색어를 입력하세요" class="search-input">
+            <!-- <div class="autocomplete-container" style="display: none;"> -->
+            <div class="autocomplete-container"></div>
         </div>
 <?php
     if(isset($_SESSION['userID'])) {
 ?>            
         <div class="login-member">
-<ul>
-    <li class="on"><a href="#"><img src="../../assets/img/profile.png" alt="profile" class="login-profile"></a></li>
-       <div  class="profile-box" id="profileMenu">
-        <ul>
-            <li>
-                 <a href="../profile/myPage.php">마이페이지</a>
-                 <a href="../login/logOutAction.php">로그아웃</a>
-            </li>
-        </ul>
-    </div>
-</ul>
-</div>
- 
-
+            <ul>
+                <li class="on"><a href="#"><img src="../../assets/img/profile.png" alt="profile" class="login-profile"></a></li>
+                <div  class="profile-box" id="profileMenu">
+                    <ul>
+                        <li>
+                            <a href="../profile/myPage.php">마이페이지</a>
+                            <a href="../login/logOutAction.php">로그아웃</a>
+                        </li>
+                    </ul>
+                </div>
+            </ul>
+        </div>
 <?php
-    }else {
+    } else {
 ?>
-        <a href="../login/login.php?action=login">로그인</a>
-        <a href="../login/login.php?action=signup">회원가입</a>
+        <a href="<?php echo $loginActionPath; ?>">로그인</a>
+        <a href="<?php echo $signupActionPath; ?>">회원가입</a>
 <?php
     }
 ?>
 
     </div>
-   
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+
+            // 검색어 입력 시 마다 자동완성 목록 가져오기
+            $('.search-input').on('input', function() {
+                var searchTerm = $(this).val();
+
+                // AJAX 요청으로 자동완성 데이터 가져오기
+                $.ajax({
+                    url: 'https://raw.githubusercontent.com/jaehyuk-lee-0712/api-pactory/main/apiData/api-data-20240603.json',
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(data) {
+                        // 검색어와 일치하는 API 이름 목록 추출
+                        var matchingAPIs = data.filter(function(item) {
+                            if(searchTerm === "" || searchTerm === null) {
+                                return "";
+                            }else  {
+                                return item.apiName.toLowerCase().includes(searchTerm.toLowerCase());
+                            }
+                        });
+
+                        // 자동완성 목록 HTML 생성
+                        var autoCompleteList = '';
+                        matchingAPIs.forEach(function(item) {
+                            autoCompleteList += '<li><a href="#" class="autocomplete-item">' + item.apiName + '</a></li>';
+                        });
+
+                        // 자동완성 목록 표시
+                        $('.autocomplete-container').html(autoCompleteList);
+                        $('.autocomplete-container').show();
+                    }
+                });
+            });
+
+            // 자동완성 목록 클릭 시 검색어 입력
+            $(document).on('click', '.autocomplete-item', function(e) {
+                e.preventDefault();
+                var selectedAPI = $(this).text();
+                $('.search-input').val(selectedAPI);
+                $('.autocomplete-container').hide();
+
+
+                // $(document).trigger('apiSelected', selectedAPI);
+                window.location.href = 'php/product/product.php?api=' + encodeURIComponent(selectedAPI);
+            });
+
+            // 검색창 바깥 클릭 시 자동완성 목록 숨기기
+            $(document).click(function(e) {
+                if (!$(e.target).closest('.search-input, .autocomplete-container').length) {
+                    $('.autocomplete-container').hide();
+                }
+            });
+        });
+    </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            // 로그인 박스
+            const profileToggle = document.querySelector(".login-member .on");
+            const profileBox = document.querySelector(".profile-box");
+
+            if (profileToggle && profileBox) {
+                profileToggle.addEventListener("click", function (event) {
+                    event.preventDefault();
+                    profileBox.classList.toggle("show");
+                });
+
+                profileBox.addEventListener("click", function () {
+                    profileBox.classList.toggle("show");
+                });
+            }
+        });
+    </script>
 </header>
--- php/product/product.php
+++ php/product/product.php
@@ -1,7 +1,3 @@
-<?php
-    include "../connection/connection.php";
-?>
-
 <!DOCTYPE html>
 <html lang="ko">
 
@@ -10,63 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>product</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    
+
 <?php
+    include "../connection/connection.php";
     include "../component/header.php";
 ?>
 
     <div id="wrap">
         <div class="product__container">
             <div class="left">
-                <div class="menu">카테고리</div>
+                <div class="menu active">카테고리</div>
                 <div class="submenu">
                     <li><a href="#" value="member">로그인</a></li>
-                    <!-- <li><a href="#">날씨</a></li> -->
                     <li><a href="#" value="delivery">배송</a></li>
                     <li><a href="#" value="calendar">달력</a></li>
                     <li><a href="#" value="bank">금융</a></li>
                     <li><a href="#" value="search">데이터 관리</a></li>
                     <li><a href="#" value="map">지도</a></li>
                 </div>
-            </div> 
+            </div>
             <div class="right">
                 <div class="right__title">
                     <a href="#" class="category">전체 보기</a>
-                    <div class="popup-btn">모달</div>
-                <select name="#" id="right__select">
-                    <option>전체</option>
-                    <option value="1">높은 관련성순</option>
-                    <option value="2">추천순</option>
-                </select>
-                </div>
-               
-                <div class="right__main column4">
-                    <!-- CSS 확인 작업용 * 지우지 마시오 * -->
-                    <!-- <div class="right__cont">
-                        <div class="right__img"></div>
-                        <div class="right__info">
-                            <a href="#">구글지도 API</a>
-                            <div class="right__category"><a href="#">지도</a></div>
-                        </div>
-                        <div class="right__btn">
-                            <div class="info__btn"><a href="#">상세 보기</a></div>
-                        </div>
-                    </div> -->
+                    <div style="position: relative; display: inline-block;" class="search__area">
+                        <input type="text" id="right__select" placeholder="검색어를 입력해주세요!">
+                        <ul class="searchresult-container" style="display: none;"></ul>
                     </div>
-                </div>    
+                </div>
+
+                <div class="right__main column4">
+                </div>
+
+                <!-- 페이징 -->
+                <div class="right__paging">
+                    <button class="first">처음으로</button>
+                    <button class="prev">이전</button>
+                    <span class="page-info"></span>
+                    <button class="next">다음</button>
+                    <button class="last">마지막으로</button>
+                </div>
+
             </div>
         </div>
         <div class="popup-view">
-            <img src="../assets/img/banner2.jpg" alt="#">
-            <h2>발주서 목록 조회</h2>
+            <img src="../../assets/img/banner2.jpg" alt="#" class="apiImg">
+            <h2 class="apiName">발주서 목록 조회</h2>
             <div>
                 <h3>설명</h3>
-                <p>발주서 목록을 하루단위 페이징 형태로 조회합니다.
+                <p class="apiDesc">발주서 목록을 하루단위 페이징 형태로 조회합니다.
                 발주서 목록을 하루단위 페이징 형태로 조회합니다.
                 </p>
             </div>
@@ -74,104 +65,318 @@
             <a href="#" class="popup-close">닫기</a>  
         </div>
     </div>
+
 <?php
-    include "../component/footer.php";
+    // include "../component/footer.php";
 ?>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
 $(document).ready(function() {
-  
-  
-  let apiData = [];
-  let categoryApiData = [];
-  let apiListData = [];
-
-  $.ajax({
-    url: "https://raw.githubusercontent.com/jaehyuk-lee-0712/api-pactory/main/apiData/api-data-20240603.json",
-    method: "GET",
-    dataType: "json",
-    success: function(data) {
-        apiData = data;
-        printApiList(data)
-    },
-    error: function() {
-      console.log("Error loading data");
-    }
-  });
-
-  function printApiList(listData) {
-    var rightMain = $(".right__main");
-    rightMain.empty();
-      for (var i = 0; i < listData.length; i++) {
-        var rightCont = $('<div class="right__cont"></div>');
-        var rightImg = $('<div class="right__img"></div>');
-        var rightInfo = $('<div class="right__info"><a href="#">'+ listData[i].apiName +'</a></div>');
-        var rightBtn = $('<div class="right__btn"><div class="info__btn"><a href="#">상세 보기</a></div></div>');
-
-        rightCont.append(rightImg, rightInfo);
-        rightCont.append(rightBtn)
-        rightMain.append(rightCont);        
-
-      }
-  }
-
-  const categoryList = $(".submenu li a");
-
-  function changeCatogory() {
-    
-    for(var i = 0; i< categoryList.length; i++) {
-        categoryList[i].addEventListener('click' , (e) => {
-            e.preventDefault();
-            categoryApiData = [];
-            const value = event.target.getAttribute('value');
-            for(let j = 0; j<apiData.length; j++) {
-                if(apiData[j].category === value) {
-                    categoryApiData.push(apiData[j]);
-                }
-            }
-            printApiList(categoryApiData);
-
-        })
-    }
-  }
-
-
+    let apiData = [];
+    let categoryApiData = [];
+    let apiListData = [];
     let currentPage = 1;
-    let itemsPerPage = 12;
+    const itemsPerPage = 12;
+
+    var mainSearchApi = new URLSearchParams(window.location.search).get('api'); // header.php search api text
+
+    const callApiData = function () {
+        $.ajax({
+        url: "https://raw.githubusercontent.com/jaehyuk-lee-0712/api-pactory/main/apiData/api-data-20240603.json",
+        method: "GET",
+        dataType: "json",
+            success: function(data) {
+                apiData = data;
+                if(mainSearchApi !== null && mainSearchApi !== "") {
+                    if(apiData.length > 0 )   {
+                        let mainSearchReulst = [];
+                        
+                        mainSearchReulst = apiData.filter(function(item) {
+                            return item.apiName.toLowerCase().includes(mainSearchApi.toLowerCase());
+                        })
+
+                        printApiList(mainSearchReulst);
+                        updatePaginationInfo(mainSearchReulst);
+
+                        mainSearchApi = ""; // 검색어에 따른 결과 한 번 출력 후 초기화 진행.
+
+                    }
+                }else if(mainSearchApi === "" || mainSearchApi === null) {
+                    printApiList(data);
+                    updatePaginationInfo(data);
+                }
+            },
+            error: function() {
+                console.log("Error loading data");
+            }
+        });
+    }
+
+    const returnApiData = function () {
+
+        let returnData = [];
+
+        $.ajax({
+        url: "https://raw.githubusercontent.com/jaehyuk-lee-0712/api-pactory/main/apiData/api-data-20240603.json",
+        method: "GET",
+        dataType: "json",
+            success: function(data) {
+                returnData = data          
+            },
+            error: function() {
+                console.log("Error loading data");
+            }
+        });
+        
+        return returnData;
+    }
 
     function printApiList(listData) {
+        
         var rightMain = $(".right__main");
         rightMain.empty();
 
         var totalPages = Math.ceil(listData.length / itemsPerPage);
         var startIndex = (currentPage - 1) * itemsPerPage;
-        var endIndex = startIndex + itemsPerPage;
+        var endIndex = Math.min(startIndex + itemsPerPage, listData.length);
 
-        for (var i = 0; i < listData.length; i++) {
+        for (var i = startIndex; i < endIndex; i++) {
+            var apiValue = "";
             var rightCont = $('<div class="right__cont"></div>');
             var rightImg = $('<div class="right__img"></div>');
             var rightInfo = $('<div class="right__info"><a href="#">' + listData[i].apiName + '</a></div>');
-            var rightBtn = $('<div class="right__btn"><div class="info__btn"><a href="#">상세 보기</a></div></div>');
+
+            apiValue = listData[i].apiName.replace(/\s/g, '');
+
+            var rightBtn = $('<div value='+ apiValue +' class="right__btn popup-btn"><div class="info__btn"><a href="#">상세 보기</a></div></div>');
 
             rightCont.append(rightImg, rightInfo);
-            rightCont.append(rightBtn)
+            rightCont.append(rightBtn);
             rightMain.append(rightCont);
-        }        
+
+            // $(".popup-btn").click(function(){
+            //     console.log($(this));
+            //     $(".popup-view").show()
+            // });
+        }
+
+        setApiModal();
     }
 
- 
-//   printApiList();
-  changeCatogory();
-});
-</script>
+    function changeCategory() {
+        var categoryList = $(".submenu li a");
+        for (var i = 0; i < categoryList.length; i++) {
+            categoryList[i].addEventListener('click', function(e) {
+                e.preventDefault();
+                categoryApiData = [];
+                const value = this.getAttribute('value');
+                for (let j = 0; j < apiData.length; j++) {
+                    if (apiData[j].category === value) {
+                        categoryApiData.push(apiData[j]);
+                    }
+                }
+                currentPage = 1; // 페이지를 첫 번째 페이지로 리셋
+                printApiList(categoryApiData);
+                updatePaginationInfo(categoryApiData);
 
+                $(".submenu li a").removeClass("active");
+                $(this).addClass("active"); 
+                $('.menu').removeClass("active");
+            });
+        }
+    }
 
-<!-- 추가된 부분 머원(17:45분) -->
-<script>
-   
-$(document).ready(function() {
-     // 마우스 오버
+    function updatePaginationInfo(list) {
+        var totalPages = Math.ceil(list.length / itemsPerPage);
+        $('.page-info').text(currentPage + ' / ' + totalPages);
+
+        if(currentPage == 1) {
+            $('.first').hide();
+            $('.prev').hide();
+        }else if(currentPage != 1) {
+            $('.first').show();
+            $('.prev').show();
+        }
+
+        if(currentPage == totalPages) {
+            $('.next').hide();
+            $('.last').hide();
+        }else if(currentPage != totalPages ) {
+            $('.next').show();
+            $('.last').show();
+        }
+    }
+
+    $('.first').click(function() {
+        currentPage = 1;
+        
+        if(categoryApiData.length > 0 ) {
+            printApiList(categoryApiData);
+            updatePaginationInfo(categoryApiData);
+        }else {
+            printApiList(apiData);
+            updatePaginationInfo(apiData);
+        }
+
+    });
+
+    $('.prev').click(function() {
+        if (currentPage > 1) {
+            currentPage--;
+            if(categoryApiData.length > 0) {
+                printApiList(categoryApiData);
+                updatePaginationInfo(categoryApiData);
+            }else {
+                printApiList(apiData);
+                updatePaginationInfo(apiData);
+            }            
+        }
+    });
+
+    $('.next').click(function() {
+        var totalPages = Math.ceil(apiData.length / itemsPerPage);
+        if (currentPage < totalPages) {
+            currentPage++;
+            
+            if(categoryApiData.length >0) {
+                printApiList(categoryApiData);
+                updatePaginationInfo(categoryApiData);
+            } else {
+                printApiList(apiData);
+                updatePaginationInfo(apiData);
+            }
+            
+        }
+    });
+
+    $('.last').click(function() {
+        var totalPages = 0;
+        if(categoryApiData.length > 0 ) {
+            totalPages = Math.ceil(categoryApiData.length / itemsPerPage);
+            currentPage = totalPages;
+            printApiList(categoryApiData);
+            updatePaginationInfo(categoryApiData);
+        }else {
+            totalPages = Math.ceil(apiData.length / itemsPerPage);
+            currentPage = totalPages;
+            printApiList(apiData);
+            updatePaginationInfo(apiData);
+        }
+    });
+    
+
+    $("#right__select").on('input' , function() {
+        let searchText = $(this).val();
+        let findApis = [];
+        if(apiData.length > 0) { // api 리스트가 있을 때만 실행.
+            if(categoryApiData.length > 0) {
+                findApis = categoryApiData.filter(function(item) {
+                    if(searchText === "" || searchText === null) {
+                        findApis = [];
+                        return "";
+                    }else {
+                        return item.apiName.toLowerCase().includes(searchText.toLowerCase());
+                    }
+                })
+
+            }else if(categoryApiData.length == 0 && apiData.length > 0) {
+                findApis = apiData.filter(function(item) {
+                    if(searchText === "" || searchText === null) {
+                        findApis = [];
+                        return "";
+                    }else {
+                        return item.apiName.toLowerCase().includes(searchText.toLowerCase());
+                    }
+                })
+            }
+
+            if(findApis.length > 0) {
+                let checkIdx = 1;
+                checkIdx++ ;
+                let searchResultList = '';
+                findApis.forEach(function(item) {
+                    searchResultList +=  '<li><a href="#" class="searchResult-item">' + item.apiName + '</a></li>';
+                })
+
+                // 자동완성 목록 표시
+                $('.searchresult-container').html(searchResultList);
+                $('.searchresult-container').show();
+            }else {
+                $('.searchresult-container').hide();
+            }
+        }   
+    });
+
+    
+    const mainSearchResult = function () {
+        if(mainSearchApi !== null || mainSearchApi !== "") {
+            let mainSearchData = returnApiData();
+        }else {
+            console.log("에러가 발생했습니다. 관리자에게 연락바랍니다.")
+        }
+    }
+
+    $(document).on('click', '.searchResult-item', function(e) {
+        e.preventDefault();
+        var selectedAPI = $(this).text();
+        let resultApis = [];
+        $('#right__select').val(selectedAPI);
+        $('.searchresult-container').hide();
+
+        if(apiData.length > 0) {
+            resultApis = apiData.filter(function(item) {
+                return item.apiName.toLowerCase().includes(selectedAPI.toLowerCase());
+            })
+
+            printApiList(resultApis);
+            updatePaginationInfo(resultApis);
+
+        }
+        
+    });
+
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.search-input, .searchresult-container').length) {
+            $('.searchresult-container').hide();
+        }
+    });
+
+    
+    function showApiDetailData(apiName) {
+        console.log(apiName)
+    }
+
+    function setApiModal() {
+        $(".popup-btn").click(function(){
+            const apiValue = $(this).attr('value');
+            
+            if(apiValue !== "" && apiValue !== null && apiValue !== undefined) {
+                if(apiData.length > 0) {
+                    console.log(apiValue);
+                    let apiDetailInfo = [];
+                    for(let i = 0; i<apiData.length; i++) {
+                        if(apiData[i].apiName.replace(/\s/g, '') === apiValue) {
+                            apiDetailInfo.push(apiData[i]);
+                        }
+                    }
+
+                    if(apiDetailInfo.length > 0) {
+                        $('.apiImg').attr('src' , apiDetailInfo[0].apiImgUrl)
+                        $('.apiDesc').text(apiDetailInfo[0].apiDesc);
+                        $('.apiName').text(apiDetailInfo[0].apiName);
+                        $('.popup-url').attr('href' , apiDetailInfo[0].apiStie)
+                    }
+                }
+
+                
+            }
+
+            $(".popup-view").show()
+        });
+    }
+
+      // 마우스 오버
     $(".right__main").on("mouseover", ".right__btn", function() {
         $(this).closest(".right__cont").css("background-color", "#8e93a5");
     }, );
@@ -180,15 +385,16 @@ $(document).ready(function() {
         $(this).closest(".right__cont").css("background-color", "");
     });
 
-    // 모달
-    $(".popup-btn").click(function(){
-        $(".popup-view").show()
-    })
     $(".popup-close").click(function(){
         $(".popup-view").hide()
     })
-});
 
+    
+
+    callApiData();
+    changeCategory();
+    // mainSearchResult();
+});
 </script>
 </body>
 </html>
